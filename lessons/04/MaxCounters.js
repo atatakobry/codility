a2809f35306858ccf41ee64fba666b35450892c0
Alexandr Loginov <atatakobry@gmail.com>
2020-09-17 16:07:08 +0300

    04 / MaxCounters (100%)

--- lessons/04/MaxCounters.js
+++ lessons/04/MaxCounters.js
@@ -1,19 +1,31 @@
 function solution(N, A) {
   const counters = Array(N).fill(0);
-  let max = 0;
+  let maxCounterToSet = 0;
+  let totallyMaxCounter = 0;
 
   for (let i = 0; i < A.length; i++) {
     if (1 <= A[i] && A[i] <= N) {
-      counters[A[i] - 1] += 1;
+      const index = A[i] - 1;
 
-      if (counters[A[i] - 1] > max) {
-        max = counters[A[i] - 1];
+      // "if A[K] = X, such that 1 ≤ X ≤ N, then operation K is increase(X)"
+      // also consider `maxCounterToSet` due to
+      // "if A[K] = N + 1 then operation K is max counter"
+      counters[index] = Math.max(counters[index], maxCounterToSet) + 1;
+
+      // find the newest totally max counter
+      if (counters[index] > totallyMaxCounter) {
+        totallyMaxCounter = counters[index];
       }
-    } else {
-      // NOTE: it's faster than Array.fill
-      for (let j = 0; j < counters.length; j++) {
-        counters[j] = max;
-      }
+    } else if (A[i] === N + 1) {
+      // update `maxCounterToSet`
+      maxCounterToSet = totallyMaxCounter;
+    }
+  }
+
+  // set all "untouched" remaining counters to `maxCounterToSet`
+  for (let j = 0; j < counters.length; j++) {
+    if (counters[j] < maxCounterToSet) {
+      counters[j] = maxCounterToSet;
     }
   }
 
