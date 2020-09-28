97ca24a95d7c85c3fb101e4813d82328376d3577
Alexandr Loginov <atatakobry@gmail.com>
2020-09-28 12:22:47 +0300

    05 / PassingCars (60%)

---
+++ lessons/05/PassingCars.js
@@ -0,0 +1,15 @@
+function solution(A) {
+  let count = 0;
+
+  for (let i = 0; i < A.length - 1; i++) {
+    if (A[i] === 0) {
+      for (let j = i + 1; j < A.length; j++) {
+        if (A[j] === 1) {
+          count += 1;
+        }
+      }
+    }
+  }
+
+  return count > 1000000000 ? -1 : count;
+}
